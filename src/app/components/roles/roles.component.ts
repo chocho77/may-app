a06999b6c32650bff8da38440646fde782454536
Chavdar Momchilov <chavmom@gmail.com>
2024-10-28 13:32:56 +0200

    On branch master  Changes to be committed: 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/app/components/designation/designation.component.css 	new file:   src/app/components/designation/designation.component.html 	new file:   src/app/components/designation/designation.component.spec.ts 	new file:   src/app/components/designation/designation.component.ts 	new file:   src/app/components/roles/roles.component.css 	new file:   src/app/components/roles/roles.component.html 	new file:   src/app/components/roles/roles.component.spec.ts 	new file:   src/app/components/roles/roles.component.ts

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  //string, number, boolean, date, object, array, null, undefined
+  firstName: string = "Angular Tutorial";
+  angularVersion = "Version 18";
+
+  version: number = 18;
+
+  isActive: boolean = false;
+
+  currentDate : Date = new Date();
+
+  
+
+}
