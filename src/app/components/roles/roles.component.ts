e8f1e5b78bbcd7890fd0a681e081679d02bc1501
Chavdar Momchilov <chavmom@gmail.com>
2024-10-28 13:45:57 +0200

    Changes to be committed: 	modified:   src/app/components/roles/roles.component.html 	modified:   src/app/components/roles/roles.component.ts

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
